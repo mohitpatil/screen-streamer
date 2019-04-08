56d0a1f43d54d1baf5438c8e1dde3ce222d4d8e2
Mohit <mohit.patil@terafinainc.com>
2019-04-07 19:40:28 -0700

    addding SMTP Server.

--- clientfront/src/component/StreamShow.js
+++ clientfront/src/component/StreamShow.js
@@ -1,20 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../action/index";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.video = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
   }
+
+  componentDidUpdate() {
+      this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+      //console.log('unmount called');
+      this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `https://youtu.be/unJzy1XwQ2k.flv`
+    });
+    this.player.attachMediaElement(this.video.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    const {title, description } = this.props.stream;
+    const { title, description } = this.props.stream;
     return (
       <div>
-        <h1>{title}</h1>
-        <h5>{description}</h5>
+        <video ref={this.video} style={{ width: "100%" }} controls />
+        <h1>Name of Stream: {title}</h1>
+        <h5>Description: {description}</h5>
       </div>
     );
   }
@@ -26,6 +56,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 export default connect(
-mapStateToProps,
+  mapStateToProps,
   { fetchStream }
 )(StreamShow);
